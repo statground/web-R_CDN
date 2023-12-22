24be5c9bc6284051ba264d928770ad69c5f893e1
Jaeseong Yoo <praster1@gmail.com>
2023-12-22 20:40:12 +0900

    가입자 수, 등급별 멤버 수

---
+++ scripts/operation/members/get_visitors_statistics_count.js
@@ -0,0 +1,24 @@
+async function get_visitors_statistics_count() {
+	function Div_statistics_count(props){
+		return(
+			<div class="p-4 bg-white rounded-lg text-center md:p-8">
+				<Div_sub_title title={"가입자 수"} />
+			
+				<div class="w-full bg-white rounded-lg shadow">
+					<dl class="grid grid-cols-4 w-full gap-8 p-4 mx-auto text-gray-900 md:grid-cols-1 md:p-8">
+						<Div_card title={"총 가입자 수"} value={props.data.cnt_total} unit={"명"} />
+						<Div_card title={"올해 가입자 수"} value={props.data.cnt_current_year} unit={"명"} sub={"작년"} value_last={props.data.cnt_last_year} />
+						<Div_card title={"이번 달 가입자 수"} value={props.data.cnt_current_month} unit={"명"} sub={"지난 달"} value_last={props.data.cnt_last_month} />
+						<Div_card title={"오늘 가입자 수"} value={props.data.cnt_current_day} unit={"명"} sub={"지난 달"} value_last={props.data.cnt_last_day} />
+					</dl>
+				</div>
+			</div>
+		)
+	}
+
+	const data = await fetch("/operation/ajax_members_statistics_count")
+					.then(res=> { return res.json(); })
+					.then(res=> { return res; });
+
+	ReactDOM.render(<Div_statistics_count data={data} />, document.getElementById("div_statistics_count"))
+}
