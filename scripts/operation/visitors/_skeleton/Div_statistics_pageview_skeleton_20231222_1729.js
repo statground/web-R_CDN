64d06cb3d28fef0dd3fd8c8b35532319dd2b4ca9
Jaeseong Yoo <praster1@gmail.com>
2023-12-22 17:29:59 +0900

    border 삭제

---
+++ scripts/operation/visitors/_skeleton/Div_statistics_pageview_skeleton_20231222_1729.js
@@ -0,0 +1,19 @@
+// 페이지 뷰 Skeleton
+function Div_statistics_pageview_skeleton() {
+	return (
+		<div class="p-4 bg-white rounded-lg text-center md:p-8 animate-pulse">
+			<Div_sub_title title={"페이지 뷰"} />
+		
+			<div class="w-full bg-white rounded-lg shadow">
+				<div class="p-4 bg-white rounded-lg text-center md:p-8">
+					<dl class="grid grid-cols-4 w-full gap-8 p-4 mx-auto text-gray-900 md:grid-cols-1 md:p-8 animate-pulse">
+						<Div_sub_card_skeleton title={"총 페이지 뷰"} />
+						<Div_sub_card_skeleton title={"올해 총 페이지 뷰"} />
+						<Div_sub_card_skeleton title={"이번 달 페이지 뷰"} />
+						<Div_sub_card_skeleton title={"오늘 페이지 뷰"} />
+					</dl>
+				</div>
+			</div>
+		</div>
+	)
+}
---
+++ scripts/operation/visitors/_skeleton/Div_statistics_visitors_skeleton_20231222_1729.js
@@ -0,0 +1,19 @@
+// 방문자 수 Skeleton
+function Div_statistics_visitors_skeleton() {
+	return (
+		<div class="p-4 bg-white rounded-lg text-center md:p-8 animate-pulse">
+			<Div_sub_title title={"방문자 수"} />
+		
+			<div class="w-full bg-white rounded-lg shadow">
+				<div class="p-4 bg-white rounded-lg text-center md:p-8">
+					<dl class="grid grid-cols-4 w-full gap-8 p-4 mx-auto text-gray-900 md:grid-cols-1 md:p-8 animate-pulse">
+						<Div_sub_card_skeleton title={"방문자 수/일"} />
+						<Div_sub_card_skeleton title={"올해 방문자 수/일"} />
+						<Div_sub_card_skeleton title={"이번 달 방문자 수/일"} />
+						<Div_sub_card_skeleton title={"오늘 방문자 수"} />
+					</dl>
+				</div>
+			</div>
+		</div>
+	)
+}
