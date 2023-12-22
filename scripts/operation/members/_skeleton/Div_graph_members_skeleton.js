a7e10607a130fc175a4252c74d5ff26fd53c8080
Jaeseong Yoo <praster1@gmail.com>
2023-12-22 20:31:47 +0900

    가입자 수 추이 스켈레톤

--- scripts/operation/members/_skeleton/Div_graph_members_skeleton.js
+++ scripts/operation/members/_skeleton/Div_graph_members_skeleton.js
@@ -1,20 +1,20 @@
-// 방문 추이 Skeleton
-function Div_graph_visitors_skeleton() {
+// 가입자 수 추이 Skeleton
+function Div_graph_members_skeleton() {
 	return (
-		<div class="p-4 bg-white rounded-lg md:p-8 text-center animate-pulse" id="div_graph_visitors_container">
-			<Div_sub_title title={"방문 추이 그래프"} />
+		<div class="p-4 bg-white rounded-lg md:p-8 text-center animate-pulse" id="div_graph_members_container">
+			<Div_sub_title title={"가입자 수 추이 그래프"} />
 		
 			<div class="mb-4 border-b border-gray-200">
 				<ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="tab_list_graph">
 					<li class="mr-2">
 						<button class={class_tab_enabled} id="tab_graph_monthly" type="button" 
-								onClick={() => get_graph_visitors('monthly')}>
+								onClick={() => get_graph_members('monthly')}>
 							월
 						</button>
 					</li>
 					<li class="mr-2">
 						<button class={class_tab_disabled} id="tab_graph_yearly" type="button"
-								onClick={() => get_graph_visitors('yearly')}>
+								onClick={() => get_graph_members('yearly')}>
 							년
 						</button>
 					</li>
