93fa540eb7cc398dbe23c27e46df8e253ae02784
JaeseongYoo <praster1@gmail.com>
2024-04-03 21:46:23 +0900

    read - set_main

---
+++ scripts/community/free/read/set_main.js
@@ -0,0 +1,72 @@
+function set_main() {
+	function Div_main() {    
+		return (
+			<div class="flex flex-col justify-center items-center py-8 px-20 w-full max-w-screen-sm mx-auto
+						md:px-8">
+				<Div_page_header title={"자유 게시판 / 묻고 답하기"} subtitle={"커뮤니티"} />
+			
+				<div class="flex flex-col justify-center items-center w-full space-y-4">
+					<div class="grid grid-cols-3 justify-center items-start w-full gap-4 md:grid-cols-1">
+						<div class="col-span-2 w-full">
+							
+							<div class="w-full" id="div_community_read_header">
+								<div class="w-full h-12 bg-gray-300 mb-4 animate-pulse"></div>
+							</div>
+							<div class="w-full" id="div_community_read_content">
+								<div class="w-full h-48 bg-gray-300 mb-4 animate-pulse"></div>
+							</div>
+							<div class="w-full" id="div_community_read_comment">
+								<div class="w-full h-24 bg-gray-300 animate-pulse"></div>
+							</div>
+						</div>
+
+						<div class="flex flex-col justify-center items-start w-full space-y-4">
+							<div class="flex flex-col justify-center items-start w-full space-y-2">
+								<div class="flex flex-col justify-center items-center w-full">
+									<button type="button" onClick={() => location.href='/community/write/'}
+											class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full
+													hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300">
+										글쓰기
+									</button>
+								</div>
+								<div class="grid grid-cols-3 justify-center items-center gap-2 w-full">
+									<button type="button" onClick={() => location.href='/community/write/'}
+										class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full
+												hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300">
+										수정
+									</button>
+									<button type="button" onClick={() => location.href='/community/write/'}
+										class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full
+												hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300">
+										삭제
+									</button>
+									<button type="button" onClick={() => location.href='/community/write/'}
+										class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full
+												hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300">
+										목록으로
+									</button>
+								</div>
+							</div>
+							
+							<Div_sidelist_skeleton id={"div_article_famous_list"} title={"최근 인기 글"} />
+							<Div_sidelist_skeleton id={"div_new_comment_list"} title={"최근 댓글"} />
+							<Div_sidelist_skeleton id={"div_my_article_list"} title={"내가 쓴 글"} />
+							<Div_sidelist_skeleton id={"div_my_comment_list"} title={"내가 쓴 댓글"} />
+							<Div_sidelist_skeleton id={"div_to_me_comment_list"} title={"나에게 쓴 댓글"} />
+
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		)
+	}
+
+	ReactDOM.render(<Div_main />, document.getElementById("div_main"))
+	get_read_article(orderID)
+	get_article_famous_list()
+	get_new_comment_list()
+	get_my_article_list()
+	get_my_comment_list()
+	get_to_me_comment_list()
+}
