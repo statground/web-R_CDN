bf7060cab8c90e33ea77b88760da7b746b2574e0
Jaeseong Yoo <praster1@gmail.com>
2024-04-14 12:38:45 +0900

    Div_new_article_list_20240414_1238

--- scripts/index/Div_new_article_list_20240407_1948.js
+++ scripts/index/Div_new_article_list_20240407_1948.js
@@ -19,14 +19,14 @@ function Div_new_article_list(props) {
 		<div class="bg-white border-b w-full">
 			<div class="flex flex-col px-6 py-4 space-y-1 cursor-pointer hover:bg-gray-100 w-full"
 				onClick={() => location.href='/' + category_menu + category_url + '/read/' + props.data.uuid + '/'}>
-				<div class="flex flex-row justify-start items-center">
+				<div class="flex flex-row justify-start items-center space-x-2">
 					<span class="font-bold text-sm w-fit max-w-9/12 truncate ...">
 						{props.data.title}
 					</span>
 
 					<Span_btn_article_new toggle={props.data.is_new} />
 					<Span_btn_article_secret toggle={props.data.is_secret} />
-
+					<Span_btn_my_article toggle={props.data.check_reader} />
 				</div>
 				<div class="flex flex-wrap justify-start items-center space-x-2">
 					<Span_btn_user user_nickname={props.data.user_nickname} role={props.data.user_role} />
