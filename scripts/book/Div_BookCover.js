6a65cf634166ce5651ba3815db7978a54a197de7
JaeseongYoo <praster1@gmail.com>
2025-09-21 02:16:13 +0900

    Div_main으로 통합

--- scripts/book/Div_BookCover.js
+++
@@ -1,9 +0,0 @@
-function Div_BookCover({ coverSrc, alt }) {
-	return (
-	  <aside className="bd-aside">
-		<div className="bd-card">
-		  <img className="bd-cover w-full rounded-lg" src={coverSrc} alt={alt} />
-		</div>
-	  </aside>
-	);
-}
